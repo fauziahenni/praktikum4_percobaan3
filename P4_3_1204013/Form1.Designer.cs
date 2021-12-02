bfbd40533ad6be234173547297db872120107feb
fauziahenni <87329906+fauziahenni@users.noreply.github.com>
2021-12-02 06:38:51 -0800

    Add files via upload

---
+++ P4_3_1204013/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace P4_3_1204013
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Huruf = new System.Windows.Forms.TextBox();
+            this.Angka = new System.Windows.Forms.TextBox();
+            this.Email = new System.Windows.Forms.TextBox();
+            this.Angka2 = new System.Windows.Forms.TextBox();
+            this.Angka1 = new System.Windows.Forms.TextBox();
+            this.epWarning = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epWrong = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epCorrect = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWrong)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Huruf  :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Angka  :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email  :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(25, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "*Angka 1>Angka 2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(25, 144);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Angka 1  :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(25, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Angka 2  :";
+            // 
+            // Huruf
+            // 
+            this.Huruf.Location = new System.Drawing.Point(138, 19);
+            this.Huruf.Name = "Huruf";
+            this.Huruf.Size = new System.Drawing.Size(100, 22);
+            this.Huruf.TabIndex = 6;
+            this.Huruf.Leave += new System.EventHandler(this.Huruf_Leave);
+            // 
+            // Angka
+            // 
+            this.Angka.Location = new System.Drawing.Point(138, 48);
+            this.Angka.Name = "Angka";
+            this.Angka.Size = new System.Drawing.Size(100, 22);
+            this.Angka.TabIndex = 7;
+            this.Angka.Leave += new System.EventHandler(this.Angka_Leave);
+            // 
+            // Email
+            // 
+            this.Email.Location = new System.Drawing.Point(138, 76);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(100, 22);
+            this.Email.TabIndex = 8;
+            this.Email.Leave += new System.EventHandler(this.Email_Leave);
+            // 
+            // Angka2
+            // 
+            this.Angka2.Location = new System.Drawing.Point(138, 172);
+            this.Angka2.Name = "Angka2";
+            this.Angka2.Size = new System.Drawing.Size(100, 22);
+            this.Angka2.TabIndex = 9;
+            this.Angka2.Leave += new System.EventHandler(this.Angka2_Leave);
+            // 
+            // Angka1
+            // 
+            this.Angka1.Location = new System.Drawing.Point(138, 141);
+            this.Angka1.Name = "Angka1";
+            this.Angka1.Size = new System.Drawing.Size(100, 22);
+            this.Angka1.TabIndex = 10;
+            this.Angka1.Leave += new System.EventHandler(this.Angka1_Leave);
+            // 
+            // epWarning
+            // 
+            this.epWarning.ContainerControl = this;
+            this.epWarning.Icon = ((System.Drawing.Icon)(resources.GetObject("epWarning.Icon")));
+            // 
+            // epWrong
+            // 
+            this.epWrong.ContainerControl = this;
+            this.epWrong.Icon = ((System.Drawing.Icon)(resources.GetObject("epWrong.Icon")));
+            // 
+            // epCorrect
+            // 
+            this.epCorrect.ContainerControl = this;
+            this.epCorrect.Icon = ((System.Drawing.Icon)(resources.GetObject("epCorrect.Icon")));
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 253);
+            this.Controls.Add(this.Angka1);
+            this.Controls.Add(this.Angka2);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.Angka);
+            this.Controls.Add(this.Huruf);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Validasi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWrong)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Huruf;
+        private System.Windows.Forms.TextBox Angka;
+        private System.Windows.Forms.TextBox Email;
+        private System.Windows.Forms.TextBox Angka2;
+        private System.Windows.Forms.TextBox Angka1;
+        private System.Windows.Forms.ErrorProvider epWarning;
+        private System.Windows.Forms.ErrorProvider epWrong;
+        private System.Windows.Forms.ErrorProvider epCorrect;
+    }
+}
+
---
+++ P4_3_1204013/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P4_3_1204013
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        
+
+        private void Huruf_Leave(object sender, EventArgs e)
+        {
+            if (Huruf.Text == "")
+            {
+                epWarning.SetError(Huruf, "Textbox Huruf tidak boleh kosong");
+
+            }
+            else
+            {
+                if ((Huruf.Text).All(Char.IsLetter))
+                {
+
+                    epCorrect.SetError(Huruf, "Betul!");
+                }
+                else
+                {
+                    epWrong.SetError(Huruf, "inputan hanya boleh huruf!");
+
+                }
+            }
+        }
+
+        private void Angka_Leave(object sender, EventArgs e)
+        {
+            if ((Angka.Text).All(Char.IsNumber))
+            {
+                epCorrect.SetError(Angka, "Betul!");
+            }
+
+            else if (Angka.Text == "")
+            {
+                epWarning.SetError(Angka, "Textbox Huruf tidak boleh kosong");
+            }
+
+            else
+            {
+                epWrong.SetError(Angka, "inputan hanya boleh Angka!");
+            }
+        }
+
+        private void Email_Leave(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(Email.Text, @"^^[^@\s]+@[^@\s]+(\.[^@\s]+)+$"))
+            {
+                epCorrect.SetError(Email, "Betul!");
+            }
+
+            else if (Email.Text == "")
+            {
+                epWarning.SetError(Email, "Textbox Huruf tidak boleh kosong");
+
+            }
+
+            else
+            {
+                epWrong.SetError(Email, "Format email salah !\nContoh: a@b.c");
+            }
+        }
+
+        private void Angka1_Leave(object sender, EventArgs e)
+        {
+            if ((Angka1.Text).All(Char.IsNumber))
+            {
+                if (Angka2.Text == "")
+                {
+                    epWarning.SetError(Angka1, "Angka 2 Belum terisi");
+                }
+
+                else
+                {
+                    epCorrect.SetError(Angka1, "Angka 2 Terisi");
+                }
+            }
+
+            else if (Angka1.Text == "")
+            {
+                epWarning.SetError(Angka1, "Textbox Huruf tidak boleh kosong");
+
+            }
+
+            else
+            {
+                epWrong.SetError(Angka1, "inputan hanya boleh Angka!");
+            }
+
+        }
+        private void Angka2_Leave(object sender, EventArgs e)
+        {
+            if ((Angka2.Text).All(Char.IsNumber))
+            {
+                int angka1 = int.Parse(Angka1.Text);
+                int angka2 = int.Parse(Angka2.Text);
+                if (angka1 > angka2)
+                {
+                    epCorrect.SetError(Angka2, "Angka 1 lebih besar dari angka 2 ");
+                }
+            }
+
+            else if ((Angka2.Text == ""))
+            {
+                epWarning.SetError(Angka2, "Textbox Huruf tidak boleh kosong");
+
+            }
+
+            else
+            {
+                epWrong.SetError(Angka2, "inputan hanya boleh Angka!");
+            }
+        }
+
+    }
+}
